e36f299aafa325ebebbf9f24e37c5c4c4c521e9c
Gabriel Lima de Souza <gabriel.souza.1354648@sga.pucminas.br>
2022-05-29 18:11:46 -0300

    Links atualizados

--- src/Backend/Models/Esporte.js
+++ src/Backend/Models/Esporte.js
@@ -1,27 +1,24 @@
-const db = require("./Db");//puxa conexão com o DataBase -> arena
+const db = require("./Db"); //puxa conexão com o DataBase -> arena
 
 // TABELA Esporte:
 
-const esporte = db.sequelize.define('esportes',{
-    
-    IdEsporte: {
-        type: db.Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
-    },
+const esporte = db.sequelize.define("esporte", {
+  IdEsporte: {
+    type: db.Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
 
-    Nome: {
-        type: db.Sequelize.STRING,
-        allowNull: false
-    },
+  Nome: {
+    type: db.Sequelize.STRING,
+    allowNull: false,
+  },
 
-    Descricao: {
-        type: db.Sequelize.TEXT,
-    }
-
-
-})
+  Descricao: {
+    type: db.Sequelize.TEXT,
+  },
+});
 
 //esporte.sync({force: true})// Mantenha-se comentado caso já exista esta tabela
 
