b5da0991a4a7e6c866d08a414e183eb206a8922c
Gabriel Lima de Souza <gabriel.souza.1354648@sga.pucminas.br>
2022-05-29 18:02:48 -0300

    Rota estática principal e realocação de pastas

---
+++ src/app.js
@@ -0,0 +1,27 @@
+// Carregando módulos
+const express = require('express')
+//const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express();
+const path = require('path')
+
+// Configurações
+    // Body Parser
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    // Handlebars
+    //app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    //app.set('view engine','handlebars')
+    // Public
+    app.use(express.static("frontend/Tela_inicial"))
+
+// Rotas
+    app.get('/', function(req,res){
+        res.sendFile(__dirname + "/frontend/Tela_inicial/index.html")
+    })
+
+// Outros
+const PORT = 8081
+app.listen(PORT,()=>{
+    console.log("Servidor rodando em http://localhost:8081")
+})
