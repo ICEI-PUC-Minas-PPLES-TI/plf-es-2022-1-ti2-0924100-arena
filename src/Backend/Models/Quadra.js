b97eee472f1ec9099fbf2619dc86d7e7761cc487
Gabriel de Souza <gabriel.souza.1365691@sga.pucminas.br>
2022-05-29 13:45:59 -0300

    Sequelize tabela quadra

---
+++ src/Backend/Models/Quadra.js
@@ -0,0 +1,39 @@
+const db = require("./Db"); //puxa conexão com o DataBase -> arena
+
+// TABELA QUADRA:
+
+const quadra = db.sequelize.define("quadra", {
+    EmailLocatario: {
+        type: db.Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+    },
+
+    CodigoQuadra: {
+        type: db.Sequelize.STRING(4),
+        primaryKey: true,
+        allowNull: false,
+    },
+
+    CEP: {
+        type: db.Sequelize.INTEGER,
+        allowNull: false
+    },
+
+    Numero: {
+        type: db.Sequelize.INTEGER,
+        allowNull: false
+    },
+
+    Capacidade: {
+        type: db.Sequelize.INTEGER,
+    },
+
+    Preco: {
+        type: db.Sequelize.DECIMAL(6, 2),
+    },
+});
+
+//atleta.sync({force: true})// Mantenha-se comentado caso já exista esta tabela
+
+module.exports = quadra;
